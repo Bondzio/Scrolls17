8c53c7bfea26eff92adc162a652c2eb1dc3950cb
akriti <akritikts@gmail.com>
2016-09-01 19:50:30 +0530

    icons added

--- app/src/main/java/in/silive/scrolls/Fragments/UploadDoc.java
+++ app/src/main/java/in/silive/scrolls/Fragments/UploadDoc.java
@@ -1,11 +1,14 @@
 package in.silive.scrolls.Fragments;
 
 
+import android.app.ProgressDialog;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import in.silive.scrolls.R;
 
@@ -13,6 +16,12 @@ import in.silive.scrolls.R;
  * A simple {@link Fragment} subclass.
  */
 public class UploadDoc extends Fragment {
+    Button upload_doc;
+    int serverResponseCode = 0;
+    ProgressDialog progressDialog = null;
+    final String uploadFilePath = "/mnt/sdcard/";
+    final String uploadFileName = "service_lifecycle.png";
+    Uri upLoadServerUri;
 
 
     public UploadDoc() {
@@ -25,6 +34,14 @@ public class UploadDoc extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_upload_doc, container, false);
+        upload_doc = (Button)v.findViewById(R.id.upload_doc);
+        //upLoadServerUri = "http://www.androidexample.com/media/UploadToServer.php";
+        upload_doc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         return v;
     }
 
