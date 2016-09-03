e620d00e6e729e7377f4731f25d4b974a5a9457a
akriti <akritikts@gmail.com>
2016-09-03 16:54:08 +0530

    REgisterFragment completed

--- app/src/main/java/in/silive/scrolls/Util/Config.java
+++ app/src/main/java/in/silive/scrolls/Util/Config.java
@@ -7,4 +7,5 @@ public class Config {
     public static final String LOG = "Scrolls";
     public static String ID_BY_EMAIL = "http://silive.in/testapi/api/Participants/GetScrollsIdByEmail?email=";
     public static String ID_BY_LOGIN = "http://silive.in/testapi/api/Participants/GetScrollsIdByEmail?email=";
+    public static String LIST_OF_COLLEGE = "";
 }
