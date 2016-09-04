1ab49e5e650e623bb5316ed44aeec14117644539
akriti <akritikts@gmail.com>
2016-09-04 19:27:16 +0530

    Get college api added

--- app/src/main/java/in/silive/scrolls/Util/Config.java
+++ app/src/main/java/in/silive/scrolls/Util/Config.java
@@ -6,7 +6,21 @@ package in.silive.scrolls.Util;
 public class Config {
     public static final String LOG = "Scrolls";
     public static final String UPLOAD_DOC = "sd";
+    public static String GET = "GET";
+    public static String POST = "POST";
+    public static String GET_COURSES = "http://akgec-scrolls.com/rest/api/Domains/GetCourses";
+    public static String CREATE_COLLEGE = "http://akgec-scrolls.com/rest/api/Colleges/CreateCollege";
+    public static String GET_COLLEGES = "http://akgec-scrolls.com/rest/api/Colleges/GetColleges";
     public static String ID_BY_EMAIL = "http://silive.in/testapi/api/Participants/GetScrollsIdByEmail?email=";
     public static String ID_BY_LOGIN = "http://silive.in/testapi/api/Participants/GetScrollsIdByEmail?email=";
     public static String LIST_OF_COLLEGE = "";
+    public static String GET_DOMAINS = "http://akgec-scrolls.com/rest/api/Domains/GetDomains";
+    public static String GET_TOPICS = "http://akgec-scrolls.com/rest/api/Domains/GetTopics?domainId=1";
+    public static String CHECK_IS_PHONE_NUMBER_REGISTERED = "isPhoneNoAlreadyRegistered";
+    public static String SELF_REGISTRATION = "http://silive.in/testapi/api/Participants/CreateParticipant";
+    public static String IS_EMAIL_ALREADY_REGISTERED = "http://silive.in/testapi/api/Participants/IsEmailAlreadyRegistered?email=";
+    public static String PREFERENCES = "PREFERENCES";
+    public static String individual_id = "individual_id";
+    public static String team_id = "team_id";
+    public static String IS_PHONE_NUMBER_REGISTERED = "http://silive.in/testapi/api/Participants/IsPhoneNoAlreadyRegistered?phoneno=";
 }
